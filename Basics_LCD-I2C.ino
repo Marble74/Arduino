9e8362ec7d7daad67b60fc53c0025e956ac964bf
Suherli <88471871+Marble74@users.noreply.github.com>
2022-12-23 16:12:30 +0700

    Update Basics_LCD-I2C.ino

--- Basics_LCD-I2C.ino
+++ Basics_LCD-I2C.ino
@@ -1,1 +1,19 @@
+//Library
+#include <LiquidCrystal_I2C.h>
+
+//Setup LCD-I2C
+LiquidCrystal_I2C (0x27,16,2);
+
+void setup()
+{
+     lcd.init();
+     lcd.backlight();
+     lcd.setCursor(0,0);
+     lcd.print("Hello World");
+     lcd.setCursor(0,1);
+     lcd.print("I'm Newbie");
+}
+void loop()
+{
+}
 
