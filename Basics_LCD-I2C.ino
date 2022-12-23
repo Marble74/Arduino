24b71b08cc44eacf87809c4736417d168e753dcc
Suherli <88471871+Marble74@users.noreply.github.com>
2022-12-23 20:08:26 +0700

    Update Basics_LCD-I2C.ino

--- Basics_LCD-I2C.ino
+++ Basics_LCD-I2C.ino
@@ -2,6 +2,12 @@
 //Created: 23 December 2022
 //Before using this code you must first know your LCD address then make sure you have installed the LiquidCrystal_I2C Library on your Arduino IDE
 
+// Arduino To LCD I2C
+// GND to GND
+// VCC to VCC
+// A4 to SDA
+// A5 to SCL
+
 //Library
 #include <LiquidCrystal_I2C.h>
 
@@ -13,7 +19,7 @@ void setup()
      lcd.init();
      lcd.backlight();
      lcd.setCursor(0,0);
-     lcd.print("Hello World");
+     lcd.print("Hello World"); 
      lcd.setCursor(0,1);
      lcd.print("I'm Newbie");
 }
